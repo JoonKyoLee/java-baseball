4cbdb4013fbf32f858ec112f5ffd24b9d3187220
JoonKyo <joonkyolee@gmail.com>
2023-10-01 01:43:14 +0900

    feat(User): 유저가 입력한 수를 저장하는 기능과 입력한 수를 리턴해주는 기능 추가

--- src/main/java/baseball/model/User.java
+++ src/main/java/baseball/model/User.java
@@ -1,2 +1,20 @@
-package baseball.model;public class User {
+package baseball.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    private List<Integer> userNumber;
+
+    public User(){
+        userNumber = new ArrayList<>();
+    }
+
+    public List<Integer> getUserNumber(){
+        return userNumber;
+    }
+
+    public void setUserNumber(List<Integer> userNumber){
+        this.userNumber = userNumber;
+    }
 }
