0811b304534ca9b5f85cc9c7bfce00b088a0feb7
JoonKyo <joonkyolee@gmail.com>
2023-10-01 01:33:22 +0900

    feat(Computer): 컴퓨터의 난수를 생성하는 기능과 리턴하는 기능 구현

---
+++ src/main/java/baseball/model/Computer.java
@@ -0,0 +1,31 @@
+package baseball.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Computer {
+    private static final int GAME_NUMBER_LENGTH = 3;
+    private static final int GAME_RANDOM_MIN_NUM = 1;
+    private static final int GAME_RANDOM_MAX_NUM = 9;
+    private List<Integer> computerNumber;
+
+    public Computer(){
+        computerNumber = new ArrayList<>();
+        setComputerNumber();
+    }
+
+    public List<Integer> getComputerNumber(){
+        return computerNumber;
+    }
+
+    public void setComputerNumber(){
+        while(computerNumber.size() < GAME_NUMBER_LENGTH){
+            int randomNumber = Randoms.pickNumberInRange(GAME_RANDOM_MIN_NUM, GAME_RANDOM_MAX_NUM);
+            if(!computerNumber.contains(randomNumber)){
+                computerNumber.add(randomNumber);
+            }
+        }
+    }
+}
