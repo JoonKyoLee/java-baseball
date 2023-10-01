053a4111c8aad304e347f23ddc135d98592d1ce0
JoonKyo <joonkyolee@gmail.com>
2023-10-01 17:01:59 +0900

    feat(OutputView): 볼, 스트라이크의 결과를 출력하는 기능 추가

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,9 +1,18 @@
 package baseball.view;
 
+import baseball.util.BallComparator;
+
+import java.util.List;
+
 public class OutputView {
     private static final String START_BASEBALL_GAME_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String FINISH_BASEBALL_GAME_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String ASK_RESTART_GAME_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String BALL_MESSAGE = "볼 ";
+    private static final String STRIKE_MESSAGE = "스트라이크";
+    private static final String NOTHING_MESSAGE = "낫싱";
+
+    private BallComparator ballComparator;
 
 
     public void printStartMessage(){
@@ -13,5 +22,25 @@ public class OutputView {
     public void printFinishMessage(){
         System.out.println(FINISH_BASEBALL_GAME_MESSAGE);
         System.out.println(ASK_RESTART_GAME_MESSAGE);
+    }
+
+    public void printBallStrikeMessage(){
+        List<Integer> baseballResult = ballComparator.getBallStrike();
+        int ball = baseballResult.get(0);
+        int strike = baseballResult.get(1);
+
+        if(ball > 0){
+            System.out.print(ball + BALL_MESSAGE);
+        }
+
+        if(strike > 0){
+            System.out.print(strike + STRIKE_MESSAGE);
+        }
+
+        if(ball == 0 && strike == 0){
+            System.out.print(NOTHING_MESSAGE);
+        }
+
+        System.out.println();
     }
 }
