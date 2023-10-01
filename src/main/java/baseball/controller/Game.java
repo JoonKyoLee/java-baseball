becaf9dff89d06a4f0df2e9b15913d826ec1372d
JoonKyo <joonkyolee@gmail.com>
2023-10-01 14:19:35 +0900

    feat(Game): 게임 초기 세팅 기능 추가

--- src/main/java/baseball/controller/Game.java
+++ src/main/java/baseball/controller/Game.java
@@ -1,2 +1,30 @@
-package baseball.controller;public class Game {
+package baseball.controller;
+
+import baseball.model.Computer;
+import baseball.model.User;
+import baseball.view.InputView;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Game {
+    private final Computer computer;
+    private final User userInput;
+    private final InputView inputView = new InputView();
+
+    private List<Integer> userInputNumber;
+    private List<Integer> computerNumber;
+
+    public Game(){
+        computer = new Computer();
+        computer.setComputerNumber();
+        userInput = new User();
+
+        userInputNumber = new ArrayList<>();
+        computerNumber = new ArrayList<>();
+
+        inputView.printStartMessage();
+    }
+
+
 }
