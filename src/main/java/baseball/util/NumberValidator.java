057248cebb1d26e243267cab369d4152b4b899f8
JoonKyo <joonkyolee@gmail.com>
2023-10-01 18:04:53 +0900

    feat(NumberValidator): 예외 처리 기능 수정

--- src/main/java/baseball/util/NumberValidator.java
+++ src/main/java/baseball/util/NumberValidator.java
@@ -9,27 +9,37 @@ public class NumberValidator {
 
     private static final String RESTART_NUMBER = "1";
     private static final String EXIT_GAME_NUMBER = "2";
+    private static final String NOT_THREE_MESSAGE = "3개의 숫자를 입력해주세요.";
+    private static final String NOT_DIFFERENT_MESSAGE = "서로 다른 숫자를 입력해주세요.";
+    private static final String NOT_IN_RANGE_MESSAGE = "1 ~ 9 사이의 숫자를 입력해주세요.";
 
-    public boolean userNumberValidator(List<Integer> userNumber){
-        return checkLength(userNumber)
-                && checkDifferentNumber(userNumber)
-                && checkNumberRange(userNumber);
+
+    public void userNumberValidator(List<Integer> userNumber){
+        checkLength(userNumber);
+        checkDifferentNumber(userNumber);
+        checkNumberRange(userNumber);
     }
 
-    public boolean checkLength(List<Integer> userNumber){
-        return userNumber.size() == LENGTH;
+    public void checkLength(List<Integer> userNumber){
+        if(userNumber.size() != LENGTH){
+            throw new IllegalArgumentException(NOT_THREE_MESSAGE);
+        }
     }
 
-    public boolean checkDifferentNumber(List<Integer> userNumber){
-        return userNumber.stream()
+    public void checkDifferentNumber(List<Integer> userNumber){
+        if(userNumber.stream()
                 .distinct()
-                .count() == LENGTH;
+                .count() != LENGTH){
+            throw new IllegalArgumentException(NOT_DIFFERENT_MESSAGE);
+        }
     }
 
-    public boolean checkNumberRange(List<Integer> userNumber){
-        return userNumber.stream()
+    public void checkNumberRange(List<Integer> userNumber){
+        if(userNumber.stream()
                 .filter(number -> USER_NUMBER_MIN_VALUE <= number && number <= USER_NUMBER_MAX_VALUE)
-                .count() == LENGTH;
+                .count() != LENGTH){
+            throw new IllegalArgumentException(NOT_IN_RANGE_MESSAGE);
+        }
     }
 
     public boolean restartNumberValidator(String number){
