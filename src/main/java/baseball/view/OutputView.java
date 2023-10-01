10cefbd6f0dac866e626b7a97303825f4cf7e12c
JoonKyo <joonkyolee@gmail.com>
2023-10-01 17:15:31 +0900

    feat(OutputView): 종료 메시지 출력하는 기능 수정

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -7,7 +7,6 @@ import java.util.List;
 public class OutputView {
     private static final String START_BASEBALL_GAME_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String FINISH_BASEBALL_GAME_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
-    private static final String ASK_RESTART_GAME_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     private static final String BALL_MESSAGE = "볼 ";
     private static final String STRIKE_MESSAGE = "스트라이크";
     private static final String NOTHING_MESSAGE = "낫싱";
@@ -21,7 +20,6 @@ public class OutputView {
 
     public void printFinishMessage(){
         System.out.println(FINISH_BASEBALL_GAME_MESSAGE);
-        System.out.println(ASK_RESTART_GAME_MESSAGE);
     }
 
     public void printBallStrikeMessage(){
