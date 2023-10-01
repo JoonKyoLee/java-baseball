72d428bd69d8d5ab32a61cda761229b9d50c11c3
JoonKyo <joonkyolee@gmail.com>
2023-10-01 15:13:52 +0900

    feat(NumberValidator): 유저가 입력한 재시작/종료 여부 문자열 유효성 검증하는 기능 추가

--- src/main/java/baseball/util/NumberValidator.java
+++ src/main/java/baseball/util/NumberValidator.java
@@ -1,14 +1,14 @@
 package baseball.util;
 
-import baseball.view.InputView;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public class NumberValidator {
     private static final int LENGTH = 3;
     private static final int USER_NUMBER_MIN_VALUE = 1;
     private static final int USER_NUMBER_MAX_VALUE = 9;
+
+    private static final String RESTART_NUMBER = "1";
+    private static final String EXIT_GAME_NUMBER = "2";
 
     public boolean userNumberValidator(List<Integer> userNumber){
         return checkLength(userNumber)
@@ -30,5 +30,9 @@ public class NumberValidator {
         return userNumber.stream()
                 .filter(number -> USER_NUMBER_MIN_VALUE <= number && number <= USER_NUMBER_MAX_VALUE)
                 .count() == LENGTH;
+    }
+
+    public boolean restartNumberValidator(String number){
+        return number.equals(RESTART_NUMBER) || number.equals(EXIT_GAME_NUMBER);
     }
 }
