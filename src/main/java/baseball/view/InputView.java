b5ae6f69b8414657f97612407ad74736bd3b9f00
JoonKyo <joonkyolee@gmail.com>
2023-10-01 16:45:27 +0900

    feat(InputView): 숫자야구 시작 메시지 삭제

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -3,13 +3,8 @@ package baseball.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    private static final String START_BASEBALL_GAME_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String INPUT_USER_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
     private static final String RESTART_GAME_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
-
-    public void printStartMessage(){
-        System.out.println(START_BASEBALL_GAME_MESSAGE);
-    }
 
     public String inputUserNumber(){
         System.out.print(INPUT_USER_NUMBER_MESSAGE);
